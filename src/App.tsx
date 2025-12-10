8b481d9f07db1156c0e92d24ef83407435e3148a
spicychimkilling-cyber <spicychimkilling@gmail.com>
2025-12-10 17:46:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from './components/ui/sonner';
 import { AuthProvider } from './contexts/AuthContext';
 import { Navbar } from './components/Navbar';
